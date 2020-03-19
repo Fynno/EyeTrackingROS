02f798b26def5a11bdc30c05527f64097bb7b638
Fynno <f.knudsen@tuhh.de>
2020-03-19 17:34:39 +0100

    New build

--- catkin_ws/build/CTestTestfile.cmake
+++ catkin_ws/build/CTestTestfile.cmake
@@ -5,7 +5,5 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 subdirs("gtest")
-subdirs("joint_state_publisher/joint_state_publisher_gui")
 subdirs("simulation")
 subdirs("arm_kinematics")
-subdirs("joint_state_publisher/joint_state_publisher")
